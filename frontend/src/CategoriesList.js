b804d8bb78f00b8b068ac7aa2ea5c81c4c909e54
Caro <stuhr.carolin@googlemail.com>
2020-07-04 15:43:34 +0200

    Adding transition

--- frontend/src/CategoriesList.js
+++ frontend/src/CategoriesList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components/macro'
+import { CSSTransition } from 'react-transition-group'
 
 export default function CategoriesList({
   category,
@@ -23,21 +24,30 @@ export default function CategoriesList({
           <ButtonStyled onClick={() => subcategoryVisible(category.id)}>
             {category.name}
           </ButtonStyled>
-          {categorySelected === category.id && isSubcategoryVisible && (
+          <CSSTransition
+            in={isSubcategoryVisible}
+            classNames="fade"
+            timeout={200}
+            unmountOnExit
+          >
             <StyledSection>
-              <ParagraphStyled onClick={() => setSubcategorySelected(0)}>
-                <LinkStyled to="/products">all</LinkStyled>
-              </ParagraphStyled>
-              {category.subcategories.map((subcategory) => (
-                <ParagraphStyled
-                  onClick={() => setSubcategorySelected(subcategory.id)}
-                  key={subcategory.id}
-                >
-                  <LinkStyled to="/products">{subcategory.name}</LinkStyled>
-                </ParagraphStyled>
-              ))}
+              {categorySelected === category.id && (
+                <>
+                  <ParagraphStyled onClick={() => setSubcategorySelected(0)}>
+                    <LinkStyled to="/products">all</LinkStyled>
+                  </ParagraphStyled>
+                  {category.subcategories.map((subcategory) => (
+                    <ParagraphStyled
+                      onClick={() => setSubcategorySelected(subcategory.id)}
+                      key={subcategory.id}
+                    >
+                      <LinkStyled to="/products">{subcategory.name}</LinkStyled>
+                    </ParagraphStyled>
+                  ))}
+                </>
+              )}
             </StyledSection>
-          )}
+          </CSSTransition>
         </>
       )}
     </>
@@ -48,8 +58,28 @@ export default function CategoriesList({
   }
 
   function subcategoryVisible(categoryId) {
-    setCategorySelected(categoryId)
+    // if (categoryId === categorySelected) {
+    //   setIsSubcategoryVisible(!isSubcategoryVisible)
+    //   setCategorySelected('')
+    // } else {
     setIsSubcategoryVisible(!isSubcategoryVisible)
+    setCategorySelected(categoryId)
+
+    // setCategorySelected(categoryId)
+
+    // if (isSubcategoryVisible.includes(categoryId)) {
+    //   const index = isSubcategoryVisible.indexOf(categoryId)
+    //   console.log(index)
+    //   console.log(isSubcategoryVisible)
+    //   setIsSubcategoryVisible([
+    //     ...isSubcategoryVisible.slice(0, index),
+    //     ...isSubcategoryVisible.slice(index + 1),
+    //   ])
+    //   console.log(index)
+    // } else {
+    //   setIsSubcategoryVisible([...isSubcategoryVisible, categoryId])
+    //   console.log(isSubcategoryVisible)
+    // }
   }
 }
 
@@ -69,6 +99,7 @@ const ButtonStyled = styled.button`
 `
 const LinkStyled = styled(Link)`
   text-decoration: none;
+  z-index: 4;
 `
 const ParagraphStyled = styled.p`
   font-size: 16px;
@@ -82,5 +113,23 @@ const StyledSection = styled.section`
   }
   p:first-of-type {
     margin-top: 4px;
+  }
+
+  &.fade-enter {
+    height: 0;
+  }
+
+  &.fade-enter-active {
+    height: 100px;
+    transition: height 200ms;
+  }
+
+  &.fade-exit {
+    height: 100px;
+  }
+
+  &.fade-exit-active {
+    height: 0;
+    transition: height 200ms;
   }
 `
